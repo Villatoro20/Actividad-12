eb87d76ffffad54c2e8cf187c1550351f8f03d81
Villatoro20 <pato83315@outlook.com>
2024-11-26 15:27:01 -0600

    On branch master Changes to be committed: 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Chuck Norris Jokes</h1>
+    </header>
+    <ul>
+      <li v-for="(joke, index) in jokes" :key="index">
+        {{ joke.value }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      jokes: [
+        { value: "Chuck Norris can skydive into outer space." },
+        { value: "The chief export of Chuck Norris is pain." },
+        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333;
+  background: #f4f4f9;
+  padding: 20px;
+}
+header {
+  background-color: #222;
+  color: #fff;
+  padding: 10px 0;
+}
+h1 {
+  margin: 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid #ccc;
+  background: #fff;
+  border-radius: 5px;
 }
 </style>
